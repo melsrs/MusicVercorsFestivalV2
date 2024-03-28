378f064810751c612c33d1b2303c1b41cf62fe6f
Mélissa <melissa.soares1909@gmail.com>
2024-03-28 15:29:43 +0100

    création models + ReservationRepository

---
+++ src/Repositories/ReservationRepository.php
@@ -0,0 +1,92 @@
+<?php
+namespace src\Repositories;
+
+use PDO;
+use PDOException;
+use src\Models\Reservation;
+use src\Models\Database;
+
+class ReservationRepository {
+  private $DB;
+
+  public function __construct()
+  {
+    $database = new Database;
+    $this->DB = $database->getDB();
+
+    require_once __DIR__ . '/../../config.php';
+  }
+
+  public function getAllReservations(){
+    $sql = "SELECT * FROM ".PREFIXE."ver_reservation;";
+
+    $retour = $this->DB->query($sql)->fetchAll(PDO::FETCH_CLASS, Reservation::class);
+
+    return $retour;
+  }
+
+  public function getThisReservationById(int $id_reservation): Reservation|bool {
+    $sql = "SELECT * FROM ".PREFIXE."ver_reservation WHERE id_reservation = :id_reservation";
+
+    $statement = $this->DB->prepare($sql);
+    $statement->bindParam(':id_reservation', $id_reservation);
+    $statement->execute();
+    $statement->setFetchMode(PDO::FETCH_CLASS, Reservation::class);
+    $retour = $statement->fetch();
+
+    return $retour;
+  }
+
+
+  public function CreateThisReservation(Reservation $Reservation): bool{
+    $sql = "INSERT INTO ". PREFIXE . "ver_reservation (id_reservation, nombre_resa, prix_total, id_user) VALUES (:id_reservation, :nombre_resa, :prix_total, :id_user)";
+
+    $statement = $this->DB->prepare($sql);
+
+    $retour = $statement->execute([
+      ':id_reservation' => $Reservation->getIdReservation(),
+      ':nombre_resa' => $Reservation->getNombreResa(),
+      ':prix_total' => $Reservation->getPrixTotal(),
+      ':id_user' => $Reservation->getIdUser()
+
+    ]);
+
+    return $retour;
+  }
+
+  public function UpdateThisReservation(Reservation $Reservation): bool{
+    $sql = "UPDATE ". PREFIXE . "ver_reservation
+            SET
+              id_reservation = :id_reservation,
+              nombre_resa = :nombre_resa,
+              id_user = :id_user
+            WHERE id_reservation = :id_reservation";
+
+    $statement = $this->DB->prepare($sql);
+
+    $retour = $statement->execute([
+        ':id_reservation' => $Reservation->getIdReservation(),
+        ':nombre_resa' => $Reservation->getNombreResa(),
+        ':prix_total' => $Reservation->getPrixTotal(),
+        ':id_user' => $Reservation->getIdUser()
+  
+      ]);
+
+    return $retour;
+  }
+
+    public function deleteThisReservation(int $ID): bool {
+      try{
+      $sql = "DELETE FROM " . PREFIXE . "relations_films_categories WHERE ID_CATEGORIES = :ID;
+              DELETE FROM " . PREFIXE . "categories WHERE ID = :ID;";
+  
+      $statement = $this->DB->prepare($sql);
+  
+      return $statement->execute([':ID' => $ID]);
+  
+      } catch(PDOException $error) {
+        echo "Erreur de suppression : " . $error->getMessage();
+        return FALSE;
+      }
+    }
+}
--- src/router.php
+++ src/router.php
@@ -1,11 +1,8 @@
 <?php
 
-
 use src\Controllers\HomeController;
 
 $HomeController = new HomeController;
-
-
 
 $route = $_SERVER['REDIRECT_URL'];
 $methode = $_SERVER['REQUEST_METHOD'];
@@ -16,6 +13,30 @@ switch ($route) {
         echo "je suis sur l'accueil";
         break;
 }
+
+
+
+// switch ($route) {
+//     case HOME_URL:
+
+//         if (isset($_SESSION['connecté'])) {
+//         header('location: '.HOME_URL.'dashboard');
+//         die;
+//         } else {
+//         $HomeController->index();
+//         }
+//         break;
+//     case HOME_URL.'test':
+//            $AllUsers= new ReservationRepository();
+//             var_dump($Reservation->getReservationID());
+
+//     default:
+//         $HomeController->page404();
+//         break;
+// }
+
+
+
 
 
 // switch ($route) {
